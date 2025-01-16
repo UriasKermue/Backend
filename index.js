e7f0e39b3f0c06ed43542f85c83b9261d5e3827a
Urias Kermue <kermueurias8@gmail.com>
2025-01-16 15:45:51 +0530

    Recent changes to the backend (Urias Kermue)

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const userRoutes = require('./routes/userRoutes'); // Ensure the path is correct
-const authRoutes = require('../Backend/routes/authRoutes'); // Ensure the path is correct
+const Newuser = require('./routes/Newuser'); // Ensure the path is correct
+const newauthRoutes = require('../Backend/routes/newauthRoutes'); // Ensure the path is correct
 const appointmentRoutes = require('./routes/appointmentRoutes'); // Import Appointment routes
 const doctorRoutes = require('../Backend/routes/doctorRoutes')
 const reminderRoutes = require('./routes/reminderRoutes');
 const cronJob = require('../Backend/Controller/Service/cron'); // Import the cron job
 const medicalRecordRoutes = require('../Backend/routes/medicalRecordRoutes'); // Import the new medical record routes
 const errorHandler = require('../Backend/middlewares/uploadMiddleware');
+const predictionRoutes = require("./routes/predictionRoutes");
 const connectDB = require('../Backend/DB'); // DB connection
 
 const app = express();
@@ -19,15 +20,18 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// path to the public directory
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 // Routes
-app.use('/api/users', userRoutes);
-app.use('/api/auth', authRoutes);
+app.use('/api/newusers', Newuser);
+app.use('/api/newauth', newauthRoutes);
 app.use('/api/appointments', appointmentRoutes); // Use Appointment routes
 app.use('/api/doctors', doctorRoutes);
 app.use('/api', reminderRoutes); // Use reminder routes
 app.use('/api/medical-records', medicalRecordRoutes);
 app.use(errorHandler); // Apply after all other routes and middlewares
-
+app.use("/api", predictionRoutes);
 
 // Log environment variables for debugging
 console.log(`MONGO_URI: ${process.env.MONGO_URI}`);
